6c8f1ac43be51dd20ee4dfb42c8d0dc3c63ac555
vitaliy-tyma <vitaliy.tyma@gmail.com>
2019-10-20 09:21:15 +0300

    AddUserService works Select with hibernate doesn't work
    JDBC select works

--- src/main/java/home/test/EnterPoint.java
+++ src/main/java/home/test/EnterPoint.java
@@ -1,16 +1,26 @@
 package home.test;
 
-import home.test.entity.User;
-import home.test.persistence.HibernateUtil;
+import home.test.model.User;
 import home.test.service.AddUserService;
-import org.hibernate.Session;
+import home.test.service.ShowAllUsersService;
 
+import java.util.List;
+
+//Hiber tests - only addUser works well
 public class EnterPoint {
 
     public static void main(String[] args) {
         System.out.println("*********** Maven + Hibernate + MySQL ***************");
 
-        AddUserService.add("Alex", "Brat");
+        AddUserService.add("Alex2", "Brat2");
+
+        //The following doesn't work
+//        List<User> list = ShowAllUsersService.showAllUsers();
+//        User w = (User) list.get(0);
+//        System.out.println(w.getFirstName());
+
+        //System.out.println(Arrays.toString(list.toArray()));
+        //list.stream().forEach();
     }
 
 }
